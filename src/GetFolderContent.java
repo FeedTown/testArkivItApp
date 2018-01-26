d6e0db7ae14bf5c87de1d04442b9ef5cd1674836
Saikat <saikat_95@hotmail.com>
2018-01-26 13:58:59 +0100

    test

---
+++ src/GetFolderContent.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GetFolderContent{
+ 
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		String filename;
+		MetadataToExcel excelCreater;
+		
+		System.out.println("Give a name for the excelfile: ");
+		filename = scan.nextLine();
+		
+		excelCreater = new MetadataToExcel(filename);
+		
+		//excelCreater.createExcel();
+		
+		scan.close();
+		
+	}
+	
+
+	
+}
