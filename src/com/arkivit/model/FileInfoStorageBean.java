bf78db54cfe72996bb93d24f31c49740ab8966a1
Saikat <saikat_95@hotmail.com>
2018-02-27 09:46:37 +0100

    File sheet bean done

---
+++ src/com/arkivit/model/FileInfoStorageBean.java
@@ -0,0 +1,195 @@
+package com.arkivit.model;
+
+public class FileInfoStorageBean {
+	
+	
+	String headerLabel, contentLabel, archiveDiareNum, archiveDiareNumDeliv, descDelivery, archiveCreator,
+	oNumArchiveCreator, delivGov, oNumDelivGov, consultantBureau, contactPersonDeliv, telContactPerson,
+	mailContactPerson, costCenter, eBillingContactPerson, archiveName, systemName, withdrawalDate,
+	comment, projectCode, accessId, batchId;
+	
+	public FileInfoStorageBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getHeaderLabel() {
+		return headerLabel;
+	}
+
+	public void setHeaderLabel(String headerLabel) {
+		this.headerLabel = headerLabel;
+	}
+
+	public String getContentLabel() {
+		return contentLabel;
+	}
+
+	public void setContentLabel(String contentLabel) {
+		this.contentLabel = contentLabel;
+	}
+
+	public String getArchiveDiareNum() {
+		return archiveDiareNum;
+	}
+
+	public void setArchiveDiareNum(String archiveDiareNum) {
+		this.archiveDiareNum = archiveDiareNum;
+	}
+
+	public String getArchiveDiareNumDeliv() {
+		return archiveDiareNumDeliv;
+	}
+
+	public void setArchiveDiareNumDeliv(String archiveDiareNumDeliv) {
+		this.archiveDiareNumDeliv = archiveDiareNumDeliv;
+	}
+
+	public String getDescDelivery() {
+		return descDelivery;
+	}
+
+	public void setDescDelivery(String descDelivery) {
+		this.descDelivery = descDelivery;
+	}
+
+	public String getArchiveCreator() {
+		return archiveCreator;
+	}
+
+	public void setArchiveCreator(String archiveCreator) {
+		this.archiveCreator = archiveCreator;
+	}
+
+	public String getoNumArchiveCreator() {
+		return oNumArchiveCreator;
+	}
+
+	public void setoNumArchiveCreator(String oNumArchiveCreator) {
+		this.oNumArchiveCreator = oNumArchiveCreator;
+	}
+
+	public String getDelivGov() {
+		return delivGov;
+	}
+
+	public void setDelivGov(String delivGov) {
+		this.delivGov = delivGov;
+	}
+
+	public String getoNumDelivGov() {
+		return oNumDelivGov;
+	}
+
+	public void setoNumDelivGov(String oNumDelivGov) {
+		this.oNumDelivGov = oNumDelivGov;
+	}
+
+	public String getConsultantBureau() {
+		return consultantBureau;
+	}
+
+	public void setConsultantBureau(String consultantBureau) {
+		this.consultantBureau = consultantBureau;
+	}
+
+	public String getContactPersonDeliv() {
+		return contactPersonDeliv;
+	}
+
+	public void setContactPersonDeliv(String contactPersonDeliv) {
+		this.contactPersonDeliv = contactPersonDeliv;
+	}
+
+	public String getTelContactPerson() {
+		return telContactPerson;
+	}
+
+	public void setTelContactPerson(String telContactPerson) {
+		this.telContactPerson = telContactPerson;
+	}
+
+	public String getMailContactPerson() {
+		return mailContactPerson;
+	}
+
+	public void setMailContactPerson(String mailContactPerson) {
+		this.mailContactPerson = mailContactPerson;
+	}
+
+	public String getCostCenter() {
+		return costCenter;
+	}
+
+	public void setCostCenter(String costCenter) {
+		this.costCenter = costCenter;
+	}
+
+	public String geteBillingContactPerson() {
+		return eBillingContactPerson;
+	}
+
+	public void seteBillingContactPerson(String eBillingContactPerson) {
+		this.eBillingContactPerson = eBillingContactPerson;
+	}
+
+	public String getArchiveName() {
+		return archiveName;
+	}
+
+	public void setArchiveName(String archiveName) {
+		this.archiveName = archiveName;
+	}
+
+	public String getSystemName() {
+		return systemName;
+	}
+
+	public void setSystemName(String systemName) {
+		this.systemName = systemName;
+	}
+
+	public String getWithdrawalDate() {
+		return withdrawalDate;
+	}
+
+	public void setWithdrawalDate(String withdrawalDate) {
+		this.withdrawalDate = withdrawalDate;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getProjectCode() {
+		return projectCode;
+	}
+
+	public void setProjectCode(String projectCode) {
+		this.projectCode = projectCode;
+	}
+
+	public String getAccessId() {
+		return accessId;
+	}
+
+	public void setAccessId(String accessId) {
+		this.accessId = accessId;
+	}
+
+	public String getBatchId() {
+		return batchId;
+	}
+
+	public void setBatchId(String batchId) {
+		this.batchId = batchId;
+	}
+	
+	
+	
+	
+
+}
