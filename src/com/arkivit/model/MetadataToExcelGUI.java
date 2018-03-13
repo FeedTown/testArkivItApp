798b80d1d0d05184438cb629a5a6253123811ad4
Saikat <saikat_95@hotmail.com>
2018-03-13 15:05:31 +0100

    Test class testbufferreaderLambda added

--- src/com/arkivit/model/MetadataToExcelGUI.java
+++ src/com/arkivit/model/MetadataToExcelGUI.java
@@ -376,6 +376,9 @@ public class MetadataToExcelGUI{
 	}
 
 	private void sortFileList() {
+		
+		fileList.sort((o1,o2) -> o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase()));
+		
 		fileList.sort(new Comparator<File>() {
 			@Override
 			public int compare(File o1, File o2) {
