83b9f41aaa513433c6bec13211574c7817f4a206
Saikat <saikat_95@hotmail.com>
2018-03-26 13:12:00 +0200

    Closing the lboffice works now

--- src/com/arkivit/model/Converter.java
+++ src/com/arkivit/model/Converter.java
@@ -1,23 +1,48 @@
 package com.arkivit.model;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Converter {
 
 	private String libreOfficeApp = "LibreOffice.app";
+	private String notaPad = "notepad.exe", libOffice="soffice.exe", osName;
 	Runtime rt = Runtime.getRuntime();
+	Process p;
 
 	public boolean openLibreOffice() {
+		
+		osName = System.getProperty("os.name");
+		
 		try 
 		{
-
-			rt.exec("open -a " + libreOfficeApp);
+			if(osName.contains("Windows"))
+			{
+				p = rt.exec("cmd /c start " + libOffice);
+			}
+			else if(osName.contains("Mac"))
+			{
+				rt.exec("open -a " + libreOfficeApp);
+			}
+			
+			
+			Thread.sleep(5000);
+			
+			
+			
+			closeLibreOffice();
+			
 			return true;
 
 		} 
 		catch (IOException e) 
 		{
 
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -28,8 +53,16 @@ public class Converter {
 	public void closeLibreOffice() {		
 		try 
 		{
-
-			rt.exec("pkill -f " + libreOfficeApp);
+			//test(p.getInputStream());
+			if(osName.contains("Windows"))
+			{
+				rt.exec("taskkill /IM soffice.bin");
+			}
+			else if(osName.contains("Mac"))
+			{
+				rt.exec("pkill -f " + libreOfficeApp);
+			}
+			
 
 		} 
 
@@ -38,7 +71,28 @@ public class Converter {
 
 			e.printStackTrace();
 		}
+		
+		//process.destroy();
 
+	}
+	
+	/*private void test(InputStream is) {
+		 InputStreamReader isr = new InputStreamReader(is);
+         BufferedReader br = new BufferedReader(isr);
+         String line=null;
+         try {
+			while ( (line = br.readLine()) != null)
+			     System.out.println("OutPut" + ">" + line);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}*/
+
+	public static void main(String[] args)
+	{
+		new Converter().openLibreOffice();
 	}
 
 }
