2ad153a78c99a31286658761d5b044edd6309c54
Saikat <saikat_95@hotmail.com>
2018-04-03 15:20:36 +0200

    ImgConverter works fine now

--- src/com/arkivit/model/ImageFileConverter.java
+++ src/com/arkivit/model/ImageFileConverter.java
@@ -1,33 +1,20 @@
 package com.arkivit.model;
 
-
-
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-/*import org.apache.batik.transcoder.TranscoderException;
-import org.apache.batik.transcoder.TranscoderInput;
-import org.apache.batik.transcoder.TranscoderOutput;
-import org.apache.batik.transcoder.image.JPEGTranscoder;
-import org.apache.batik.transcoder.image.PNGTranscoder;*/
+import java.io.InputStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-
 import net.sf.image4j.codec.ico.ICODecoder;
 
 
 public class ImageFileConverter {
 
 
-	static MetadataToExcelGUI model;
+	private MetadataToExcelGUI model;
 	private String fileNameWithOutExt = "";
 
 
@@ -44,26 +31,27 @@ public class ImageFileConverter {
 				BufferedImage bi = ImageIO.read(new File(s.getAbsolutePath()));
 				ImageIO.write(bi, "jpeg", new File(s.getParentFile().getAbsoluteFile(), fileNameWithOutExt + ".jpeg"));
 
-
-
 				System.out.println("Image " + s.getName() + " was converted succesfully.");
+				storeOriginalImages1(s);
 			}
-
 			else if(s.getName().endsWith(".ico") || s.getName().endsWith(".ICO")) {
-				List<BufferedImage> bi = ICODecoder.read(new File(s.getAbsolutePath()));
+				
+				InputStream inputStream = new FileInputStream(s);
 				System.out.println("reading2");
-				ImageIO.write(bi.get(0), "png", new File(s.getParentFile().getAbsoluteFile(), fileNameWithOutExt + ".png"));
+				ImageIO.write(ICODecoder.read(inputStream).get(0), "png", new File(s.getParentFile().getAbsoluteFile(), fileNameWithOutExt + ".png"));
 				System.out.println("Ico was converted.");
+				inputStream.close();
+				
+				storeOriginalImages1(s);
+				
 			}
 
 			else {
 				System.out.println("no files converted");
 			}
 
-
 		}
 
-		storeOriginalImages();
 
 	}
 
@@ -73,12 +61,34 @@ public class ImageFileConverter {
 					|| s.getName().endsWith(".jpg") || s.getName().endsWith(".BMP") 
 					|| s.getName().endsWith(".bmp") || s.getName().endsWith(".ico") || s.getName().endsWith(".ICO")) {
 				System.out.println("in copy else");
-				File illegalExtension = new File(s.getAbsolutePath());
+				//File illegalExtension = new File(s.getAbsolutePath());
+
+				File illegalExtension = s;
 				File illegalExtensionDest = new File(model.getTargetexcelFilepath() + "/" + model.getFolderName() + "_img_backup");
 				System.out.println(illegalExtension + "/" + illegalExtensionDest);
-				FileUtils.moveFileToDirectory(illegalExtension, illegalExtensionDest, true);
+				try {
+
+					FileUtils.moveFileToDirectory(illegalExtension, illegalExtensionDest, true);
+				}catch(IOException e)
+				{
+					e.printStackTrace();
+				}
 			}
 		}
 
 	}
+
+	public void storeOriginalImages1(File illegalExtension) throws IOException 
+	{
+		File illegalExtensionDest = new File(model.getTargetexcelFilepath() + "/" + model.getFolderName() + "_img_backup");
+		System.out.println(illegalExtension + "/" + illegalExtensionDest);
+		try {
+			FileUtils.moveFileToDirectory(illegalExtension, illegalExtensionDest, true);
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
 }
